bbeb6d33532ff835d63712e1ba09c96dbdfe1d02
Suraj Jayakumar <106909560+SurajJayakumar@users.noreply.github.com>
2022-07-12 12:24:32 +0530

    Delete from deepface import DeepFace.py

--- from deepface import DeepFace.py
+++
@@ -1,2 +0,0 @@
-from deepface import DeepFace
-verification = DeepFace.verify(img1_path = r"C:\Users\suraj\Saved Games\ML\Photos-001\20220618_073433.jpg", img2_path = r"C:\Users\suraj\Saved Games\ML\Photos-001\20220618_073550.jpg")
