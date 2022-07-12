14010cfacd0c3faf04eba3b81046c9c7a48db6bb
Suraj Jayakumar <106909560+SurajJayakumar@users.noreply.github.com>
2022-07-12 12:22:13 +0530

    Add files via upload

---
+++ from deepface import DeepFace.py
@@ -0,0 +1,2 @@
+from deepface import DeepFace
+verification = DeepFace.verify(img1_path = r"C:\Users\suraj\Saved Games\ML\Photos-001\20220618_073433.jpg", img2_path = r"C:\Users\suraj\Saved Games\ML\Photos-001\20220618_073550.jpg")
